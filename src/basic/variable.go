c152cd8cbe4a3a413b1dda0af2f4c553f313b921
yyyfff43 <248037973@qq.com>
2022-01-01 00:44:48 +0800

    字符串练习

--- src/basic/variable.go
+++ src/basic/variable.go
@@ -1,11 +1,14 @@
-// Copyright 2021 The Go Authors. All rights reserved.
+// Copyright 2021 The Go Authors Yangfan. All rights reserved.
 
 /*
 go语言工程项目测试，包含一些基本的变量方法调用和处理
 */
 package basic
 
-import "fmt"
+import (
+	"fmt"
+	"unicode/utf8"
+)
 
 // RangeStringByByte 方法将输入的字符串已字节形式遍历
 // 将传入的字符串打印，无返回值
@@ -13,5 +16,24 @@ func RangeStringByByte(str string) {
 	for _, b := range []byte(str) {
 		fmt.Printf("%X", b)
 	}
+    fmt.Println()
+}
 
+// RangeStringByByte 方法将输入的字符串已字节形式遍历
+// 将传入的字符串打印，无返回值
+func RangeStringByRune(str string) {
+	for i, ch := range str { // ch is a rune
+		fmt.Printf("(%d %X) ", i, ch)
+	}
+	fmt.Println()
+}
+
+func RangeEachUtf8Ch(str string) {
+	bytes := []byte(str)
+	for len(bytes) > 0 {
+		ch, size := utf8.DecodeRune(bytes)
+		bytes = bytes[size:]
+		fmt.Printf("*%c* ", ch)
+	}
+	fmt.Println()
 }
--- src/main.go
+++ src/main.go
@@ -1,13 +1,34 @@
+/*
+go语言工程项目测试入口main函数
+*/
 package main
 
 import (
 	"fmt"
 	"go_test_project/src/basic"
+	"unicode/utf8"
 )
+
 
 func main() {
 	fmt.Println("项目文件入口")
 
 	var str string = "First of all，这是一个关于go语言的项目工程的测试，I try to test it"
+	fmt.Println("byte格式遍历每一个字符")
 	basic.RangeStringByByte(str)
+    fmt.Println("rune格式遍历每一个字符")
+	basic.RangeStringByRune(str)
+
+	//统计字符串包括中文在内一共多少个字，每一个英文或者特殊符号都算
+	fmt.Println("Rune count:",utf8.RuneCountInString(str))
+
+	fmt.Println("遍历UTF8字符串中的每一个字符")
+	basic.RangeEachUtf8Ch(str)
+
+	for i, ch := range []rune(str) {
+		fmt.Printf("(%d %c) ", i, ch)
+	}
+	fmt.Println()
+	var strShow []rune = []rune(str)
+	fmt.Printf("%c",strShow[1])
 }
