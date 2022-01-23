6c3e9a99b0495b8fe26e5c131103c48cd7bb734b
yyyfff43 <248037973@qq.com>
2022-01-23 11:24:38 +0800

    增加插入法实现的链表逆序

--- src/algorithm/linkList.go
+++ src/algorithm/linkList.go
@@ -10,9 +10,27 @@ type LNode struct {
 	Next *LNode
 }
 
-//节点反转
+//
+//  Reverse
+//  @Description: 节点反转(插入法，比掉头法效率高，不保存前驱结点地址，比递归法效率高，因为不需要递归调用消耗时间复杂度)
+//  @param node 链表结点
+//
 func Reverse(node *LNode){
-
+     if node == nil || node.Next == nil{
+		 return
+	 }
+	 var cur *LNode
+     var next *LNode
+     cur = node.Next.Next
+     //设置链表第一个结点为尾结点
+     node.Next.Next = nil
+     //把遍历到的结点插入到头结点的后面
+     for cur != nil {
+     	next = cur.Next
+     	cur.Next = node.Next
+     	node.Next = cur
+     	cur = next
+	 }
 }
 
 //创建链表
--- src/algorithm/linkList_test.go
+++ src/algorithm/linkList_test.go
@@ -18,3 +18,11 @@ func TestPrintNode(t *testing.T) {
 	CreateNode(&node,20)
 	PrintNode("遍历链表：",&node)
 }
+
+//测试插入法链表逆序
+func TestReverse(t *testing.T) {
+	node := LNode{}
+	CreateNode(&node,10)//创建一个1到10位值得链表
+	Reverse(&node)
+	PrintNode("遍历逆序后的链表：",&node)
+}
