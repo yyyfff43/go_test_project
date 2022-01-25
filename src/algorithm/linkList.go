325f30a503afea4fc59d12610a1e3a409830ba01
yyyfff43 <248037973u@qq.com>
2022-01-25 22:55:04 +0800

    理解带头节点的单向链表的构成

--- src/algorithm/linkList.go
+++ src/algorithm/linkList.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-//链表定义
+//链表结点定义
 type LNode struct {
 	Data interface{}
 	Next *LNode
@@ -12,7 +12,7 @@ type LNode struct {
 
 //
 //  Reverse
-//  @Description: 节点反转(插入法，比掉头法效率高，不保存前驱结点地址，比递归法效率高，因为不需要递归调用消耗时间复杂度)
+//  @Description: 带头结点的节点反转(插入法，比掉头法效率高，不保存前驱结点地址，比递归法效率高，因为不需要递归调用消耗时间复杂度)
 //  @param node 链表结点
 //
 func Reverse(node *LNode){
@@ -33,9 +33,14 @@ func Reverse(node *LNode){
 	 }
 }
 
-//创建链表
+//
+//  CreateNode
+//  @Description: 创建带头结点引导的链表，即head->1->2->3......
+//  @param node 结点结构体
+//  @param max 最大节点数
+//
 func CreateNode(node *LNode, max int) {
-	cur := node
+	cur := node //定义头结点（注意不是那个1的结点，而是设定的head引导结点，但head结点也包括在最终节点链表里）
 	for i := 1; i < max; i++ {
 		cur.Next = &LNode{}
 		cur.Next.Data = i
@@ -43,9 +48,15 @@ func CreateNode(node *LNode, max int) {
 	}
 }
 
-//打印链表的方法
+//
+//  CreateNode
+//  @Description: 打印链表
+//  @param string 提示信息
+//  @param node 链表结构体
+//
 func PrintNode(info string, node *LNode) {
 	fmt.Print(info)
+	//因为头结点head不输出，所以cur直接赋值为头结点的下一个结点，即值为1那个结点
 	for cur := node.Next; cur != nil; cur = cur.Next {
 		fmt.Print(cur.Data, " ")
 	}
